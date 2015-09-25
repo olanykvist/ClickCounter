895f90eb8b212665643105fdb62d35c3442b3e98
Ola Nykvist <ola@olanykvist.se>
2015-09-25 09:17:01 +0200

    Initial commit

---
+++ Hooks/MouseHook.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ClickCounter.Hooks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Runtime.InteropServices;
+
+    public delegate void ClickEventHandler(object sender, EventArgs e);
+
+    public class MouseHook
+    {
+        public event ClickEventHandler Click;
+
+        protected void OnClick(EventArgs e)
+        {
+            if (Click != null)
+            {
+                Click(this, e);
+            }
+        }
+    }
+}
