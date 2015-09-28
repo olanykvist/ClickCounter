3f75b034cfb6d2a5845913819850f2e01701100a
Ola Nykvist <ola@olanykvist.se>
2015-09-28 16:10:00 +0200

    Added MSLLHookStruct

---
+++ Hooks/MSLLHookStruct.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ClickCounter.Hooks
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MSLLHookStruct
+    {
+        public Point pt;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        IntPtr dwExtraInfo;
+    }
+}
